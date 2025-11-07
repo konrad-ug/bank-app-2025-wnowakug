7d2055b7c93ecdfbcf4bfd7d12414c99bb317909
wnowakug <w.nowak.223@studms.ug.edu.pl>
2025-11-07 14:20:10 +0100

    dodanie pierwszego labu

--- src/account.py
+++ src/account.py
@@ -1,4 +1,23 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, balance, pesel,promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
+        self.balance = balance
+        self.pesel = pesel
+        self.promo_code = promo_code
+
+    def pesel_check(self):
+        if len(str(self.pesel)) != 11:
+            self.pesel = "invalid"
+
+    def rabat_check(self):
+        if self.promo_code != None and self.promo_code.startswith("PROM_"):
+            self.balance += 50
+
+    def rabat_wiek_check(self):
+        p = str(self.pesel)
+
+        if int(p[0]) >= 6 and int(p[2]) <= 1 or int(p[2]) >= 2:
+            return True
+        else:
+            return False
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,6 +3,24 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
-        assert account.first_name == "John"
-        assert account.last_name == "Doe"
+        account = Account("John", "Doe",0,69093089679,"PROM_ABC")
+        assert account.first_name == "John", "nieprawidlowe imie"
+        assert account.last_name == "Doe", "nieprawidlowe nazwisko"
+        assert account.balance == 0, "niezerowy balans konta"
+        assert account.pesel == 69093089679, "nieprawidłowy pesel"
+
+    def test_pesel_length(self):
+        account2 = Account("Jane","Doe",0,97698039096)
+        assert len(str(account2.pesel)) == 11, "nieprawidlowa dlugosc peselu"
+
+    def test_empty_code(self):
+        account3 = Account("Wojtek", "Nowak", 50,12345678901)
+        assert account3.balance ==0, "naliczono rabat mimo nie podania kodu"
+
+    def test_wrong_code(self):
+        account4 = Account("Franek","Czeczotka",50,11342547342,"kod_promocyjny_lol")
+        assert account4.promo_code.startswith("PROM_"), "naliczono rabat mimo błędnego kodu"
+
+    def test_no_ammount_added(self):
+        account5 = Account("Marcin","Martychewicz",50,70031618343,"PROM_CDA")
+        assert account5.promo_code.startswith("PROM_") and account5.balance != 0, "nie naliczono rabatu mimo poprawnego kodu"
